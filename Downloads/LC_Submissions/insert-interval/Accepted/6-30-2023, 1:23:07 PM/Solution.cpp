1f5f8c356aa3414839ba6fb131675edaf78ac24a
Prince yadav <princeyadav00785@gmail.com>
2023-11-17 10:27:22 +0530

    Committing changes in folder: insert-interval/

---
+++ Downloads/LC_Submissions/insert-interval/Accepted/6-30-2023, 1:23:07 PM/Solution.cpp
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/insert-interval
+
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        intervals.push_back(newInterval);
+        sort(intervals.begin(), intervals.end());
+
+        vector<vector<int>> ans;
+        int start = intervals[0][0];
+        int end = intervals[0][1];
+
+        for (int i = 1; i < intervals.size(); i++) {
+            if (intervals[i][0] <= end) {
+                end = max(intervals[i][1], end);
+            } else {
+                ans.push_back({start, end});
+                start = intervals[i][0];
+                end = intervals[i][1];
+            }
+        }
+
+        ans.push_back({start, end});
+
+        return ans;
+    }
+};
