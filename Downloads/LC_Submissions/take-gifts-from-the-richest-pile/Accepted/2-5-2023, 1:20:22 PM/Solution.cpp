93bdf7f5c77b7f5c227a0f673bd11d1abfd7a3f8
Prince yadav <princeyadav00785@gmail.com>
2023-11-17 10:31:47 +0530

    Committing changes in folder: take-gifts-from-the-richest-pile/

---
+++ Downloads/LC_Submissions/take-gifts-from-the-richest-pile/Accepted/2-5-2023, 1:20:22 PM/Solution.cpp
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/take-gifts-from-the-richest-pile
+
+class Solution {
+public:
+    long long pickGifts(vector<int>& gifts, int k) {
+        for(int i=0;i<k;i++){
+            sort(gifts.begin(),gifts.end());
+            gifts[gifts.size()-1]= pow(gifts[gifts.size()-1],0.5);
+            
+        }
+  long long  int sum=0;
+    
+   for(int i=0;i<gifts.size();i++){
+       sum += gifts[i];
+       // cout<<gifts[i]<<" ";
+   }
+
+    return sum;
+}
+};
+
+
+
+
+// 4 9 25 64 100
+// 4  9   5  8   10
