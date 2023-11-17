a2a7fe3935106535fbebf9b13229da679bb6bd1e
Prince yadav <princeyadav00785@gmail.com>
2023-11-17 10:26:40 +0530

    Committing changes in folder: double-a-number-represented-as-a-linked-list/

---
+++ Downloads/LC_Submissions/double-a-number-represented-as-a-linked-list/Accepted/8-13-2023, 8:13:13 AM/Solution.cpp
@@ -0,0 +1,53 @@
+// https://leetcode.com/problems/double-a-number-represented-as-a-linked-list
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    
+     ListNode* reverseList(ListNode* head) {        
+        if(head==NULL||head->next==NULL){return head;}
+        ListNode* prev=NULL;
+        ListNode* temp= head;
+       while(temp!=NULL){
+           ListNode* nxt= temp->next;
+           temp->next=prev;
+           prev=temp;
+           temp=nxt;
+       }
+        return prev;
+        
+       
+    }
+    ListNode* doubleIt(ListNode* head) {
+        head = reverseList(head);
+        ListNode* l1=head;
+        ListNode* l2=head;
+         ListNode* dummy = new ListNode(-1);
+        ListNode* ans = dummy;
+        int carry = 0, sum = 0;
+        while (l1 || l2 || carry) {
+            sum = carry;
+            if (l1) {
+                sum += l1->val;
+                l1 = l1->next;
+            }
+            if (l2) {
+                sum += l2->val;
+                l2 = l2->next;
+            }
+            carry = sum / 10;
+            dummy->next = new ListNode(sum % 10);
+            dummy = dummy->next;
+        }
+        return reverseList(ans->next);
+    }
+};
---
+++ Downloads/LC_Submissions/double-a-number-represented-as-a-linked-list/Runtime Error/8-13-2023, 8:08:45 AM/Solution.cpp
@@ -0,0 +1,41 @@
+// https://leetcode.com/problems/double-a-number-represented-as-a-linked-list
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* doubleIt(ListNode* head) {
+        int ans =0;
+        ListNode* temp=head;
+        while(temp!=NULL){
+            ans= ans*10+temp->val;
+            temp=temp->next;
+        }
+        ans=ans*2;
+        string s= to_string(ans);
+        int n= s.length();
+        
+        ListNode* dummy = new ListNode(-1);
+        ListNode* a= dummy;
+        int i=0;
+        while(n--){
+            char c = s[i++];
+            string ss="";
+            ss= ss+c;
+            cout<<c<<" ";
+                int a= stoi(ss);
+            ListNode* node = new ListNode(a);
+            dummy->next =node;
+            dummy=dummy->next;
+        }
+        return a->next;
+    }
+};
