1535a7cb7e71b493dd3066aa8607bfa2f0e6e200
Prince yadav <princeyadav00785@gmail.com>
2023-11-17 10:30:33 +0530

    Committing changes in folder: reduce-array-size-to-the-half/

---
+++ Downloads/LC_Submissions/reduce-array-size-to-the-half/Accepted/6-5-2023, 2:35:50 PM/Solution.cpp
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/reduce-array-size-to-the-half
+
+class Solution {
+public:
+    int minSetSize(vector<int>& arr) {
+        map<int,int>mp;
+        for(int i=0;i<arr.size();i++){
+            mp[arr[i]]++;
+        }
+        priority_queue<int>q;
+        for(auto x:mp){
+            q.push(x.second);
+        }
+        int cnt=0,sum=0;
+        while(sum<arr.size()/2){
+            sum+=q.top();
+            cnt++;
+            q.pop();
+        }
+        
+        
+        return cnt;
+    }
+};
