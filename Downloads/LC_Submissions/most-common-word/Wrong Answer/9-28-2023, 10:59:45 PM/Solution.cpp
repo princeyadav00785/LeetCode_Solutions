ba5997e3ffcf5bef249c0cf953d6ec3d3613f8cd
Prince yadav <princeyadav00785@gmail.com>
2023-11-17 10:29:39 +0530

    Committing changes in folder: most-common-word/

---
+++ Downloads/LC_Submissions/most-common-word/Wrong Answer/9-28-2023, 10:59:45 PM/Solution.cpp
@@ -0,0 +1,36 @@
+// https://leetcode.com/problems/most-common-word
+
+class Solution {
+public:
+    string mostCommonWord(string p, vector<string>& b) {
+       map<string ,int>mp;
+        vector<string>v;
+        string s="";
+        for(int i=0;i<p.length();i++){
+            if(p[i]==' '||p[i]==','||p[i]=='.'){
+                if(s!="")
+                    transform(s.begin(), s.end(), s.begin(), ::tolower);
+                v.push_back(s);
+                s="";
+            }
+            else s+=p[i];
+        }
+        for(int i=0;i<v.size();i++){
+            mp[v[i]]++;
+        }
+        int mxm=0;
+        string t="";
+        map<string ,int>mp2;
+        for(int i=0;i<b.size();i++){
+            mp2[b[i]]++;
+        }
+        for(auto x:mp){
+            if(x.second>mxm){
+                string ss=x.first;
+                if(mp2[ss]==0)
+                t= x.first; mxm=x.second;
+            }
+        }
+       return t;
+    }
+};
