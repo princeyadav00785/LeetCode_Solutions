470890ca04661316da9a339d40f8c01415331e72
Prince yadav <princeyadav00785@gmail.com>
2023-11-17 10:28:52 +0530

    Committing changes in folder: maximum-value-of-an-ordered-triplet-i/

---
+++ Downloads/LC_Submissions/maximum-value-of-an-ordered-triplet-i/Accepted/10-4-2023, 1:06:31 AM/Solution.cpp
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/maximum-value-of-an-ordered-triplet-i
+
+class Solution {
+public:
+    long long maximumTripletValue(vector<int>& nums) {
+        long long ans=0,a=0;
+        for(int i=0;i<nums.size()-2;i++){
+            for(int j=i+1;j<nums.size()-1;j++){
+                for(int k=j+1;k<nums.size();k++){
+                    a=1ll*(nums[i] - nums[j]) * nums[k];
+                    ans= max(a,ans);
+                }
+            }
+        }
+        
+        return ans;
+        
+    }
+};
