338ecbdbc2cb1486bb12ad63948f0823de19cbbd
Prince yadav <princeyadav00785@gmail.com>
2023-11-17 10:25:46 +0530

    Committing changes in folder: build-array-from-permutation/

---
+++ Downloads/LC_Submissions/build-array-from-permutation/Accepted/1-1-2023, 11:57:17 PM/Solution.cpp
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/build-array-from-permutation
+
+class Solution {
+public:
+    vector<int> buildArray(vector<int>& nums) {
+        vector<int> v(nums.size(),0);
+        for(int i =0; i<nums.size();i++){
+            v[i]=nums[nums[i]];
+        }
+        return v;
+    }
+};
