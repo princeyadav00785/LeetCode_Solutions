b0a0582caa0b09432525cb81c86fa3c2ffe487c9
Prince yadav <princeyadav00785@gmail.com>
2023-11-17 10:29:14 +0530

    Committing changes in folder: minimum-limit-of-balls-in-a-bag/

---
+++ Downloads/LC_Submissions/minimum-limit-of-balls-in-a-bag/Accepted/8-6-2023, 11:17:51 PM/Solution.cpp
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/minimum-limit-of-balls-in-a-bag
+
+class Solution {
+public:
+    int minimumSize(vector<int>& A, int k) {
+        int left = 1, right = 1e9;
+        while (left < right) {
+            int mid = (left + right) / 2, count = 0;
+            for (int a : A)
+                count += (a - 1) / mid;
+            if (count > k)
+                left = mid + 1;
+            else
+                right = mid;
+        }
+        return left;
+    }
+};
+
+
+  
