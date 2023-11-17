a79106315d12a91e2a0e1d59874680d6ee453975
Prince yadav <princeyadav00785@gmail.com>
2023-11-17 10:26:11 +0530

    Committing changes in folder: convert-the-temperature/

---
+++ Downloads/LC_Submissions/convert-the-temperature/Accepted/2-11-2023, 12:34:09 PM/Solution.cpp
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/convert-the-temperature
+
+class Solution {
+public:
+    vector<double> convertTemperature(double celsius) {
+        vector<double> ans(2,0);
+        ans[0]=celsius+273.15;
+        ans[1]=celsius*1.80+32.00;
+
+        return ans;
+    }
+};
