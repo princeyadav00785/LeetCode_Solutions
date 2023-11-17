68f2b1803ffffa3dead59c853587a5bbb15e4e76
Prince yadav <princeyadav00785@gmail.com>
2023-11-17 10:27:22 +0530

    Committing changes in folder: intersection-of-two-arrays-ii/

---
+++ Downloads/LC_Submissions/intersection-of-two-arrays-ii/Wrong Answer/10-2-2022, 1:59:31 PM/Solution.cpp
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/intersection-of-two-arrays-ii
+
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> a;
+        for(int i = 0; i< nums1.size(); i++){
+            for(int j = 0; j < nums2.size(); j++){
+                if(nums1[i]==nums2[j]){
+                 a.push_back(nums1[i]);
+                    break;
+                }
+            }
+        }
+        return a;
+    }
+};
