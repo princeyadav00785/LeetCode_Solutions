7a1a890cc79175e3af7861c0792d5f373bb4e298
Prince yadav <princeyadav00785@gmail.com>
2023-11-17 10:31:36 +0530

    Committing changes in folder: subsets/

---
+++ Downloads/LC_Submissions/subsets/Accepted/7-18-2023, 9:08:29 PM/Solution.cpp
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/subsets
+
+class Solution {
+public:
+   void fxn(vector<int>&nums,int ind,vector<int> v,vector<vector<int>>&ans){
+        if(ind==nums.size()){
+            ans.push_back(v);
+            return ;
+        }
+        v.push_back(nums[ind]);
+        fxn(nums,ind+1,v,ans);
+        v.pop_back();
+        fxn(nums,ind+1,v,ans);
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        
+        vector<int>v;
+        vector<vector<int>>ans;
+        fxn(nums,0,v,ans);
+        return ans;
+    }
+};
