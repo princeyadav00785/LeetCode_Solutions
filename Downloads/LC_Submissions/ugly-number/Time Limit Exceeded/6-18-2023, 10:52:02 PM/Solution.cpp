d573f8e5b412167a0002b14e53e43663c180996d
Prince yadav <princeyadav00785@gmail.com>
2023-11-17 10:31:56 +0530

    Committing changes in folder: ugly-number/

---
+++ Downloads/LC_Submissions/ugly-number/Time Limit Exceeded/6-18-2023, 10:52:02 PM/Solution.cpp
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/ugly-number
+
+class Solution {
+public:
+    bool isUgly(int num) {
+    for (int i=2; i<6 ; i++)
+    while (num % i == 0)
+        num /= i;
+return num == 1;
+    }
+};
