179d60b6aa687af1fe67e4b9728d51a1ebffeb72
Prince yadav <princeyadav00785@gmail.com>
2023-11-17 10:26:08 +0530

    Committing changes in folder: construct-the-longest-new-string/

---
+++ Downloads/LC_Submissions/construct-the-longest-new-string/Accepted/6-25-2023, 10:02:46 AM/Solution.cpp
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/construct-the-longest-new-string
+
+class Solution {
+public:
+    int longestString(int x, int y, int z) {
+        long long int ans=z;
+        if(y>x){
+            ans+= 2*x+1;
+        }else if(x>y)ans +=2*y+1;
+        else ans+=2*x;
+        return 2*ans;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  X  Y       Z
+// AA BB AA BB AA AB
+// BB AA BB AA BB AB
