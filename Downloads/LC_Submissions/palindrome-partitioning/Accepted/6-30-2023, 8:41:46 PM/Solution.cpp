d22e0c0a3462e5e732435bee97ba5aa4ca5bdab1
Prince yadav <princeyadav00785@gmail.com>
2023-11-17 10:30:03 +0530

    Committing changes in folder: palindrome-partitioning/

---
+++ Downloads/LC_Submissions/palindrome-partitioning/Accepted/6-30-2023, 8:41:46 PM/Solution.cpp
@@ -0,0 +1,34 @@
+// https://leetcode.com/problems/palindrome-partitioning
+
+class Solution {
+public:
+    bool issafe(int index,int i,string s){
+        while(index<=i){
+            if(s[index++]!=s[i--]){
+                return false;
+            }
+        }
+        return true;
+    }
+    void part(vector<string>&path,vector<vector<string>>&ans,string s,int index){
+        if(index==s.size()){
+            ans.push_back(path);
+            return;
+        }
+        for(int i=index;i<s.size();i++)
+        {
+        if(issafe(index,i,s)){
+        path.push_back(s.substr(index,i-index+1));
+        part(path,ans,s,i+1);
+        path.pop_back();
+        }
+        }
+    }
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>>ans;
+        vector<string>path;
+        part(path,ans,s,0);
+        return ans;
+    }
+    
+};
