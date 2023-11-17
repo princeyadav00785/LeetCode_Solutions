4f0dc1cc8f6e6d981f518a2481089873d70491a8
Prince yadav <princeyadav00785@gmail.com>
2023-11-17 10:25:23 +0530

    Committing changes in folder: add-to-array-form-of-integer/

---
+++ Downloads/LC_Submissions/add-to-array-form-of-integer/Runtime Error/2-12-2023, 2:37:36 PM/Solution.cpp
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/add-to-array-form-of-integer
+
+class Solution {
+public:
+    vector<int> addToArrayForm(vector<int>& num, int k) {
+        long long int ans =0;
+        // int a=1;
+        for(int i=0;i<num.size();i++){
+             ans =ans*10;
+            ans+=num[i];
+           
+        }
+        ans=ans+k;
+        cout<<ans;
+      vector<int> v;
+      int d=0;
+       while(ans>0){
+           d=ans%10;
+           v.push_back(d);
+           ans=ans/10;
+       }
+       vector v2(v.size(),0);
+       for(int i=0;i<v.size();i++){
+           v2[i]=v[v.size()-i-1];
+       }
+
+
+    
+    return v2;
+    }
+};
