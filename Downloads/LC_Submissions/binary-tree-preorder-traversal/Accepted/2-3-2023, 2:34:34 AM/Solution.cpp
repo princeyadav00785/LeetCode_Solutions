0f74ff1186f0811631f50a24c5a924a28f5c9c13
Prince yadav <princeyadav00785@gmail.com>
2023-11-17 10:25:42 +0530

    Committing changes in folder: binary-tree-preorder-traversal/

---
+++ Downloads/LC_Submissions/binary-tree-preorder-traversal/Accepted/2-3-2023, 2:34:34 AM/Solution.cpp
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/binary-tree-preorder-traversal
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    public: 
+    void traversal(TreeNode* root , vector<int> &v){
+        if(root==NULL){
+            return ;
+        }
+          v.push_back(root->val);
+           traversal(root->left,v);
+           traversal(root->right,v);
+    }
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> x;
+        traversal(root,x);
+        return x;
+    }
+};
