341601b91a7ac747f81347642f01992d6970cf69
Prince yadav <princeyadav00785@gmail.com>
2023-11-17 10:31:03 +0530

    Committing changes in folder: running-sum-of-1d-array/

---
+++ Downloads/LC_Submissions/running-sum-of-1d-array/Accepted/2-11-2023, 11:30:29 AM/Solution.cpp
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/running-sum-of-1d-array
+
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        vector<int> v(nums.size(),0);
+        int sum =0;
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+            v[i]=sum;
+        }
+        return v;
+    }
+};
