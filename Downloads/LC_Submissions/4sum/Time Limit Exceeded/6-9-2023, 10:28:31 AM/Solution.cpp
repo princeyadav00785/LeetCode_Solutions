ede4fd81b50ed66aead0972bcb474c505268cd81
Prince yadav <princeyadav00785@gmail.com>
2023-11-17 10:25:21 +0530

    Committing changes in folder: 4sum/

---
+++ Downloads/LC_Submissions/4sum/Time Limit Exceeded/6-9-2023, 10:28:31 AM/Solution.cpp
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/4sum
+
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        vector<vector<int>>ans;
+       if(nums.size()==1)return ans;
+        vector<int>v;
+        for(int i=0;i<nums.size()-3;i++){
+            for(int j=i+1;j<nums.size()-2;j++){
+                for(int k=j+1;k<nums.size()-1;k++){
+                    for(int l=k+1;l<nums.size();l++){
+                        if(nums[i]+nums[j]+nums[k]+nums[l]==target){
+                            v.push_back(nums[i]);
+                               v.push_back(nums[j]);
+                               v.push_back(nums[k]);
+                               v.push_back(nums[l]);
+                            ans.push_back(v);
+                            v.clear();
+                        }
+                    }
+                }
+            }
+        }
+         set<vector<int>>s(ans.begin(),ans.end());
+        vector<vector<int>>a(s.begin(),s.end());
+        return a;
+    }
+};
