29fd3308bd9f9844b37ad47e8961a007d33e73d3
Prince yadav <princeyadav00785@gmail.com>
2023-11-17 10:27:10 +0530

    Committing changes in folder: first-missing-positive/

---
+++ Downloads/LC_Submissions/first-missing-positive/Accepted/11-1-2023, 10:15:30 AM/Solution.cpp
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/first-missing-positive
+
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int ans=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]>(ans+1)){
+                return ans+1;
+            }
+            else{
+                ans=max(ans,nums[i]);
+            }
+            // cout<<ans<<" ";
+        }
+        return ans+1;
+    }
+};
