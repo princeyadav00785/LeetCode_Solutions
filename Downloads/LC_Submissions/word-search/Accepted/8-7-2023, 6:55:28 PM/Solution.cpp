edcf54e24f98a7ec5e2de4a44b39dc09feb49854
Prince yadav <princeyadav00785@gmail.com>
2023-11-17 10:32:09 +0530

    Committing changes in folder: word-search/

---
+++ Downloads/LC_Submissions/word-search/Accepted/8-7-2023, 6:55:28 PM/Solution.cpp
@@ -0,0 +1,36 @@
+// https://leetcode.com/problems/word-search
+
+class Solution {
+public:
+ 
+    bool fxn(vector<vector<char>>& v, string w, int a, int i, int j) {
+    if (i < 0 || j < 0 || i >= v.size() || j >= v[0].size() || v[i][j] != w[a]||v[i][j]=='!')
+        return false;
+
+    if (a == w.size() - 1)
+        return true;
+
+    char c = v[i][j];
+    v[i][j] = '!'; // Mark the current cell as visited
+
+    bool m = fxn(v, w, a + 1, i + 1, j); // Down
+    bool b = fxn(v, w, a + 1, i - 1, j); // Up
+    bool l = fxn(v, w, a + 1, i, j + 1); // Right
+    bool d = fxn(v, w, a + 1, i, j - 1); // Left
+
+    v[i][j] = c; // Restore the original character
+
+    return m || b || l || d;
+}
+    bool exist(vector<vector<char>>& v, string w) {
+        
+        for(int i=0;i<v.size();i++){
+            for(int j=0;j<v[0].size();j++){
+                if(v[i][j]==w[0]){
+                    if(fxn(v,w,0,i,j))return true;
+                }
+            }
+        }
+        return false;
+    }
+};
