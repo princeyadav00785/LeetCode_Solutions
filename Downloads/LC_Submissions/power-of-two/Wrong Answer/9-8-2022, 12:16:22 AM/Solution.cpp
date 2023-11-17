a9cf408a0fba567a86e37c018db0b3d7c6d7ce3b
Prince yadav <princeyadav00785@gmail.com>
2023-11-17 10:30:25 +0530

    Committing changes in folder: power-of-two/

---
+++ Downloads/LC_Submissions/power-of-two/Wrong Answer/9-8-2022, 12:16:22 AM/Solution.cpp
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/power-of-two
+
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+       if(n<0){
+          
+           return false;
+        }else if(n==1){
+               return true;
+        }else if((n& (n-1)) ==0){
+               return true;
+        }else    return false;
+        return 0;
+    }
+};
