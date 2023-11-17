6715fbaf94b93b25cabaf8d255691a8ce9878c13
Prince yadav <princeyadav00785@gmail.com>
2023-11-17 10:29:36 +0530

    Committing changes in folder: minimum-time-to-complete-trips/

---
+++ Downloads/LC_Submissions/minimum-time-to-complete-trips/Wrong Answer/8-6-2023, 11:02:51 PM/Solution.cpp
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/minimum-time-to-complete-trips
+
+class Solution {
+public:
+     bool fxn(vector<int>&nums,int k , int a){
+        
+        long long  cnt=0;
+        for(int i=0;i<nums.size();i++){
+            cnt+=a/nums[i];
+        }
+        if(cnt>=k)return true;
+        return false;
+    }
+    
+    long long minimumTime(vector<int>& nums, int k) {
+         int mxm=1e9;
+        
+        int l=1,h=mxm;
+        while(l<=h){
+            int mid=(l+h)/2;
+            if(fxn(nums,k,mid)){
+                h=mid-1;
+            }else {
+                l=mid+1;
+            }
+        }
+        return l;   
+    }
+};
