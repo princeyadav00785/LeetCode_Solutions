6a3f38a320bfe362369980ffb9c4897e81f32755
Prince yadav <princeyadav00785@gmail.com>
2023-11-17 10:29:43 +0530

    Committing changes in folder: next-greater-element-ii/

---
+++ Downloads/LC_Submissions/next-greater-element-ii/Accepted/6-13-2023, 9:25:21 AM/Solution.cpp
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/next-greater-element-ii
+
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>ans(n,-1);
+        stack<int>st;
+        for(int i=n-1;i>-1;i--){
+            st.push(nums[i]);
+        }
+        // int a= nums[n-1];
+        // for(int i=0;i<n;i++ ){
+        //     if(nums[i]>a){
+        //         a=nums[i];
+        //         break;
+        //     }
+        // }
+        // if(a!=nums[n-1]){st.push(a);}
+        
+        for(int i=n-1;i>-1;i--){
+            while(!st.empty()&&nums[i]>=st.top()){
+                st.pop();
+            }
+            if(!st.empty()){ans[i]=st.top();}
+            st.push(nums[i]);
+        }
+        return ans;
+    }
+};
