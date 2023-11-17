a1a7bea8269043e6afb8b1e0d3897b3e04ea9aa6
Prince yadav <princeyadav00785@gmail.com>
2023-11-17 10:28:06 +0530

    Committing changes in folder: majority-element/

---
+++ Downloads/LC_Submissions/majority-element/Accepted/7-4-2023, 9:54:35 AM/Solution.cpp
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/majority-element
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int cnt =0 ;
+        int ele;
+        for(int i = 0;i<nums.size();i++){
+            if(cnt==0){
+                ele = nums[i];
+                cnt = 1;
+            }
+            else if(ele==nums[i])cnt++;
+            else cnt--;
+        }
+        return ele;
+    }
+};
