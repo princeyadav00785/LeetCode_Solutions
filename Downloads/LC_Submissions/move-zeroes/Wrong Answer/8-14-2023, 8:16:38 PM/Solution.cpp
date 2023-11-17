aa8adc6a4bdfc5ec21a554ddf26451c1274c372a
Prince yadav <princeyadav00785@gmail.com>
2023-11-17 10:29:40 +0530

    Committing changes in folder: move-zeroes/

---
+++ Downloads/LC_Submissions/move-zeroes/Wrong Answer/8-14-2023, 8:16:38 PM/Solution.cpp
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/move-zeroes
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int start=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==0){
+                start=i;
+                break;
+            }
+        }
+      if(start==nums.size()-1)return ;
+        int second=start+1;
+        while(second<nums.size()){
+            if(nums[second]!=0){
+                nums[start++]=nums[second];
+                nums[second]=0;
+            }
+            second++;
+        }
+        
+    }
+};
