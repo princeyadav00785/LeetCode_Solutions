0a9a5a0cbd149ed559f2d895fbb0e726f48ffd88
Prince yadav <princeyadav00785@gmail.com>
2023-11-17 10:27:20 +0530

    Committing changes in folder: implement-magic-dictionary/

---
+++ Downloads/LC_Submissions/implement-magic-dictionary/Wrong Answer/10-20-2023, 10:45:58 PM/Solution.cpp
@@ -0,0 +1,37 @@
+// https://leetcode.com/problems/implement-magic-dictionary
+
+class MagicDictionary {
+public:
+     map<string,int>mp;
+    MagicDictionary() {
+       
+    }
+    
+    void buildDict(vector<string> d) {
+        for(int i=0;i<d.size();i++){
+            mp[d[i]]++;
+        }
+    }
+    
+    bool search(string W) {
+        int mnm=INT_MAX;
+        for(auto x:mp){
+           if(W.length()==x.first.length()){
+               int cnt=0;
+               for(int i=0;i<W.length();i++){
+                   if(W[i]!=x.first[i])cnt++;
+               }
+               mnm=min(mnm,cnt);
+           } 
+                      }
+        if(mnm==1)return true;
+        return false;
+    }
+};
+
+/**
+ * Your MagicDictionary object will be instantiated and called as such:
+ * MagicDictionary* obj = new MagicDictionary();
+ * obj->buildDict(dictionary);
+ * bool param_2 = obj->search(searchWord);
+ */
