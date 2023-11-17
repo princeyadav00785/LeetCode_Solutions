a358161530b60369da36857f7e201e99e4c6a07c
Prince yadav <princeyadav00785@gmail.com>
2023-11-17 10:27:52 +0530

    Committing changes in folder: longest-increasing-subsequence-ii/

---
+++ Downloads/LC_Submissions/longest-increasing-subsequence-ii/Time Limit Exceeded/7-21-2023, 9:13:58 AM/Solution.cpp
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/longest-increasing-subsequence-ii
+
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums, int k) {
+            vector<int>dp(nums.size(),1);
+        for(int i=0;i<nums.size();i++){
+            for(int j=0;j<i;j++){
+                if(nums[i]-nums[j]<=k&&nums[i]>nums[j]){
+                    dp[i]= max(dp[i],1+dp[j]);
+                }
+            }
+        }
+        
+         int mxm=INT_MIN;
+        for(int i=0;i<nums.size();i++){
+            if(dp[i]>mxm)mxm=dp[i];
+        }
+        return mxm;
+    }
+};
