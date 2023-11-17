774be66577386c65cab72f1f100dd7fdde63a1d5
Prince yadav <princeyadav00785@gmail.com>
2023-11-17 10:25:48 +0530

    Committing changes in folder: calculate-digit-sum-of-a-string/

---
+++ Downloads/LC_Submissions/calculate-digit-sum-of-a-string/Accepted/10-27-2023, 12:04:09 AM/Solution.cpp
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/calculate-digit-sum-of-a-string
+
+class Solution {
+public:
+    string digitSum(string s, int k) {
+    
+        while(s.length()>k){
+            string temp="";
+            int cnt=0,sum=0;
+            for(int i=0;i<s.length();i++){
+                cnt++;
+                sum+=s[i]-'0';
+                if(cnt==k||i==s.length()-1){
+                  temp+= to_string(sum);
+                    sum=0;
+                    cnt=0;
+                }
+            }
+            s=temp;
+        }
+        return s;
+    }
+};
