38b98f3f8f4588375ea6f26520f97ec80716fe83
Prince yadav <princeyadav00785@gmail.com>
2023-11-17 10:31:36 +0530

    Committing changes in folder: subarray-sum-equals-k/

---
+++ Downloads/LC_Submissions/subarray-sum-equals-k/Accepted/6-20-2023, 11:27:03 AM/Solution.java
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/subarray-sum-equals-k
+
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        Map<Integer,Integer> p = new HashMap();
+        
+     p.put(0,1);   
+int ans=0,sum=0;
+        
+     for(int i=0;i<nums.length;i++){
+         sum+=nums[i];
+        if(p.containsKey(sum-k)){
+            ans+=p.get(sum-k);
+        } 
+         p.put(sum,p.getOrDefault(sum,0)+1);
+     }   
+       return ans;   
+    }
+}
---
+++ Downloads/LC_Submissions/subarray-sum-equals-k/Wrong Answer/6-20-2023, 10:57:14 AM/Solution.cpp
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/subarray-sum-equals-k
+
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int cnt=0;
+        int sum=0;
+        for(int i=0;i<nums.size();i++){
+             sum+=nums[i];
+            if(sum==k){cnt++;sum=0; sum+=nums[i];}
+            if(sum>k){sum=0; sum+=nums[i];}
+           
+        }
+        if(sum==k)cnt++;
+        return cnt;
+    }
+};
