2f0595ab1d7ddfe8efc58bbd6b9854e2d050476d
Prince yadav <princeyadav00785@gmail.com>
2023-11-17 10:29:17 +0530

    Committing changes in folder: minimum-number-of-operations-to-make-arrays-similar/

---
+++ Downloads/LC_Submissions/minimum-number-of-operations-to-make-arrays-similar/Accepted/6-18-2023, 7:33:18 PM/Solution.cpp
@@ -0,0 +1,37 @@
+// https://leetcode.com/problems/minimum-number-of-operations-to-make-arrays-similar
+
+class Solution {
+public:
+    long long makeSimilar(vector<int>& a, vector<int>& b) {
+    vector<int>c;
+        vector<int>d;
+        vector<int>c1;
+        vector<int>d1;
+        for(int i=0;i<a.size();i++){
+            if(a[i]%2==0){
+                c.push_back(a[i]);
+            }
+            if(a[i]%2!=0){
+                d.push_back(a[i]);
+            }
+             if(b[i]%2==0){
+                c1.push_back(b[i]);
+            }
+            if(b[i]%2!=0){
+                d1.push_back(b[i]);
+            }
+        }
+        sort(c.begin(),c.end());
+          sort(c1.begin(),c1.end());
+          sort(d.begin(),d.end());
+          sort(d1.begin(),d1.end());
+        long long  cnt=0;
+        for(int i=0;i<c.size();i++){
+            cnt+=abs(c[i]-c1[i]);
+        }
+        for(int i=0;i<d.size();i++){
+            cnt+=abs(d[i]-d1[i]);
+        }
+        return cnt/4;
+    }
+};
