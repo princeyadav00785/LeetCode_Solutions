493e7daadd6f61b56e494b97f7f109d1b2274e33
Prince yadav <princeyadav00785@gmail.com>
2023-11-17 10:31:48 +0530

    Committing changes in folder: to-lower-case/

---
+++ Downloads/LC_Submissions/to-lower-case/Accepted/4-18-2023, 11:53:44 AM/Solution.cpp
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/to-lower-case
+
+class Solution {
+public:
+    string toLowerCase(string s) {
+       string ans="";
+        for(int i=0;i<s.length();i++)
+        {ans+= tolower(s[i]);}
+        return ans;
+    }
+};
