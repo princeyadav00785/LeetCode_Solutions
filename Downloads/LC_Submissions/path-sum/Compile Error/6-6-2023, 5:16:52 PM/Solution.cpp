d084b9e2fb108135adb2afff90225caa84997ee1
Prince yadav <princeyadav00785@gmail.com>
2023-11-17 10:30:13 +0530

    Committing changes in folder: path-sum/

---
+++ Downloads/LC_Submissions/path-sum/Compile Error/6-6-2023, 5:16:52 PM/Solution.cpp
@@ -0,0 +1,50 @@
+// https://leetcode.com/problems/path-sum
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void fxn(TreeNode*root , vector<int>&sum,vector<int>& s){
+        if(root==NULL){
+          int a=0;
+            for(int i=0;i<s.size();i++){
+                a+=s[i];
+            }
+            sum.push_back(a);
+            return;
+        }
+        s.push_back(root->val);
+        fxn(root->left,sum,s);
+        fxn(root->right,sum,s);
+        s.pop_back();
+            
+        
+    }
+    
+    bool hasPathSum(TreeNode* root, int targetSum) {
+//         if(root==NULL&&targetSum==0){return false;}
+        
+//       vector<int>sum;
+//              vector<int> s;
+//         fxn(root,sum,s);
+//         for(int i=0;i<sum.size();i++){
+//             if(sum[i]==targetSum){return true;}
+//             cout<<sum[i]<<" ";
+//         }
+        
+//         return false;
+        if(root == NULL)  return false;
+    if(root->right == root->left)  return sum == root->val;
+    return hasPathSum(root->left, sum - root->val) || hasPathSum(root->right, sum - root->val) ;
+
+    }
+};
---
+++ Downloads/LC_Submissions/path-sum/Wrong Answer/6-6-2023, 5:12:15 PM/Solution.cpp
@@ -0,0 +1,47 @@
+// https://leetcode.com/problems/path-sum
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void fxn(TreeNode*root , vector<int>&sum,vector<int>& s){
+        if(root==NULL){
+          int a=0;
+            for(int i=0;i<s.size();i++){
+                a+=s[i];
+            }
+            sum.push_back(a);
+            return;
+        }
+        s.push_back(root->val);
+        fxn(root->left,sum,s);
+        fxn(root->right,sum,s);
+        s.pop_back();
+            
+        
+    }
+    
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if(root==NULL&&targetSum==0){return true;}
+         if(root==NULL&&targetSum!=0){return true;}
+        
+      vector<int>sum;
+             vector<int> s;
+        fxn(root,sum,s);
+        for(int i=0;i<sum.size();i++){
+            if(sum[i]==targetSum){return true;}
+            cout<<sum[i]<<" ";
+        }
+        
+        return false;
+    }
+};
