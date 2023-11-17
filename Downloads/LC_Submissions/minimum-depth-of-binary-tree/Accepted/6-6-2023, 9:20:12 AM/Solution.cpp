bfaa1e9bc95919aec07bec8803c30a83b139b658
Prince yadav <princeyadav00785@gmail.com>
2023-11-17 10:29:13 +0530

    Committing changes in folder: minimum-depth-of-binary-tree/

---
+++ Downloads/LC_Submissions/minimum-depth-of-binary-tree/Accepted/6-6-2023, 9:20:12 AM/Solution.cpp
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/minimum-depth-of-binary-tree
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if(root==NULL){
+            return 0;
+        }
+        int l= minDepth(root->left);
+        int r= minDepth(root->right);
+        
+       if(l==0&&r!=0){return 1+r;}
+        else if(r==0&&l!=0){return 1+l;}
+        else return 1+min(l,r);
+    }
+};
