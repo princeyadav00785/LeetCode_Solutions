4631c48e042cba2b8a4ab28c8e27737c31c1046b
Prince yadav <princeyadav00785@gmail.com>
2023-11-17 10:26:29 +0530

    Committing changes in folder: design-hashset/

---
+++ Downloads/LC_Submissions/design-hashset/Accepted/2-17-2023, 11:42:23 PM/Solution.cpp
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/design-hashset
+
+class MyHashSet {
+public:
+    vector<int> ans;
+    MyHashSet() {
+        ans.resize(1e6+1,false);
+    }
+    
+    void add(int key) {
+        ans[key]=1;
+    }
+    
+    void remove(int key) {
+        ans[key]=-1;
+    }
+    
+    bool contains(int key) {
+    if(ans[key]==1){
+        return true;
+    }else return false;
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
