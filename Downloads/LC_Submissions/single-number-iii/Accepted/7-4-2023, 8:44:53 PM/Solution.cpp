09e863d895ce7ed0e17aeed9c6437cf2fe41862a
Prince yadav <princeyadav00785@gmail.com>
2023-11-17 10:31:15 +0530

    Committing changes in folder: single-number-iii/

---
+++ Downloads/LC_Submissions/single-number-iii/Accepted/7-4-2023, 8:44:53 PM/Solution.cpp
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/single-number-iii
+
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+            unordered_map<int, int> m;
+        vector<int>ans;
+        for(auto x: nums){
+            m[x]++;
+        }
+
+        for(auto x: m){
+            if(x.second == 1){
+                ans.push_back(x.first);
+            }
+        }
+        
+        return ans;
+    }
+};
