1aa9fc699d81c1a8a460060b3ad32af1ce03cbab
Prince yadav <princeyadav00785@gmail.com>
2023-11-17 10:28:41 +0530

    Committing changes in folder: maximum-spending-after-buying-items/

---
+++ Downloads/LC_Submissions/maximum-spending-after-buying-items/Accepted/11-12-2023, 5:10:01 PM/Solution.cpp
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/maximum-spending-after-buying-items
+
+class Solution {
+public:
+    long long maxSpending(vector<vector<int>>& values) {
+        vector<int>v;
+        for(int i=0;i<values.size();i++){
+            for(int j=0;j<values[0].size();j++){
+                v.push_back(values[i][j]);
+            }
+        }
+        sort(v.begin(),v.end());
+        long long ans=0;
+        for(int i=0;i<v.size();i++){
+            ans+=1ll*(i+1)*v[i];
+        }
+        return ans;
+    }
+};
