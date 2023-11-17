bf76d0928caec4d2949c5ba87f1e59f710bc9885
Prince yadav <princeyadav00785@gmail.com>
2023-11-17 10:28:37 +0530

    Committing changes in folder: maximum-number-of-groups-entering-a-competition/

---
+++ Downloads/LC_Submissions/maximum-number-of-groups-entering-a-competition/Wrong Answer/6-19-2023, 12:59:56 AM/Solution.cpp
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/maximum-number-of-groups-entering-a-competition
+
+class Solution {
+public:
+    int maximumGroups(vector<int>& v) {
+        set<int> s(v.begin(),v.end());
+        int n = s.size();
+        int k=0;
+        if(s.size()==1)return 1;
+        for(int i=0;i<n;i++){
+            if( (i*(i+1))/2 >n){break;}
+            else k=i; 
+        }
+        return k;
+    }
+};
+// 3 5 6 7 10 12
+//  3    5 6   7 10 12
