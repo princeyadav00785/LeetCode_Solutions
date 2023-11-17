10a668d90bc1aac3e3b8de017382259db81cf6e3
Prince yadav <princeyadav00785@gmail.com>
2023-11-17 10:25:49 +0530

    Committing changes in folder: categorize-box-according-to-criteria/

---
+++ Downloads/LC_Submissions/categorize-box-according-to-criteria/Runtime Error/1-7-2023, 8:22:19 PM/Solution.cpp
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/categorize-box-according-to-criteria
+
+class Solution {
+public:
+    
+    string categorizeBox(int length, int width, int height, int mass) {
+        int a=0;
+        int b=0;
+        string ans;
+        if(length>=10000||width>=10000||height>=10000||mass>=10000||(length*width*height)>=1000000000){
+           a=1;
+            
+        }
+       else if(mass>=100){
+           b=1;
+           
+        }
+        if(a==1&&b==1){
+             ans = "Both";
+        }else if(b==1&&a!=1){
+            ans = "Heavy";
+        }else if(a==1&&b!=1){
+           ans ="Bulky";
+        }else if(a!=1&&b!=1){
+            ans ="Neither";
+        }
+        return ans;
+    }
+};
