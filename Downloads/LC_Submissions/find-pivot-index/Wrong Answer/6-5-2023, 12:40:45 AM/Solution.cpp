b91ab7c9ee7e8f9d59a8e33208a1f352f412db61
Prince yadav <princeyadav00785@gmail.com>
2023-11-17 10:26:58 +0530

    Committing changes in folder: find-pivot-index/

---
+++ Downloads/LC_Submissions/find-pivot-index/Wrong Answer/6-5-2023, 12:40:45 AM/Solution.cpp
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/find-pivot-index
+
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+      int t=0 ,sum=0 ,index=-1;
+        for(int i=0;i<nums.size();i++){
+            t+=nums[i];
+        }
+        for(int i=0;i<nums.size();i++){
+           t-=nums[i];
+            if(sum==t){index=i;}
+                 sum+=nums[i];
+        }
+        return index;
+    }
+};
