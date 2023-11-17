b24097624a37d89175fcfa787e5e0a3963c3194a
Prince yadav <princeyadav00785@gmail.com>
2023-11-17 10:29:28 +0530

    Committing changes in folder: minimum-path-sum/

---
+++ Downloads/LC_Submissions/minimum-path-sum/Accepted/7-1-2023, 12:14:41 PM/Solution.cpp
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/minimum-path-sum
+
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+       int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        dp[0][0]=grid[0][0];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                int down = INT_MAX;
+                int right= INT_MAX;
+                if(i==0&&j==0)dp[i][j]=grid[0][0];
+                else{
+                if(i-1>-1){down =  dp[i-1][j];}
+                if(j-1>-1)right= dp[i][j-1];
+                dp[i][j]= grid[i][j]+ min(right,down);}
+            }
+            
+        }
+        return dp[n-1][m-1];
+    }
+};
