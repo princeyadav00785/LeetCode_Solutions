408824455ead988bc8fd479729ce4a057797263a
Prince yadav <princeyadav00785@gmail.com>
2023-11-17 10:26:15 +0530

    Committing changes in folder: count-distinct-numbers-on-board/

---
+++ Downloads/LC_Submissions/count-distinct-numbers-on-board/Accepted/1-30-2023, 7:24:08 PM/Solution.cpp
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/count-distinct-numbers-on-board
+
+class Solution {
+public:
+    int distinctIntegers(int n) {
+        if(n==1){return 1;}else
+        return n-1;
+        
+        
+//         9 2 4 3 5 6 7 
+    }
+};
---
+++ Downloads/LC_Submissions/count-distinct-numbers-on-board/Wrong Answer/1-30-2023, 7:23:13 PM/Solution.cpp
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/count-distinct-numbers-on-board
+
+class Solution {
+public:
+    int distinctIntegers(int n) {
+        return n-1;
+        
+        
+//         9 2 4 3 5 6 7 
+    }
+};
