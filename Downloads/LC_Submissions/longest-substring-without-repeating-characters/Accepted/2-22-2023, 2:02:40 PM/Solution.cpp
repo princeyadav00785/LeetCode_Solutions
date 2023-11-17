636051f83150bfb75e49ad4c53eece92db421b58
Prince yadav <princeyadav00785@gmail.com>
2023-11-17 10:27:59 +0530

    Committing changes in folder: longest-substring-without-repeating-characters/

---
+++ Downloads/LC_Submissions/longest-substring-without-repeating-characters/Accepted/2-22-2023, 2:02:40 PM/Solution.cpp
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/longest-substring-without-repeating-characters
+
+class Solution {
+public:
+    
+    
+//     a b c a d e f g h 
+    int lengthOfLongestSubstring(string s) {
+        map<char,int> mp;
+        int cnt =0;
+        int ans =0;
+        for(int i=0;i<s.length();i++){
+             
+            for(int j=i;j<s.length();j++){
+                if(mp.find(s[j])==mp.end()){
+                mp[s[j]]++;
+                cnt++;
+            }else {
+                cnt =0;
+                // cnt++;
+                mp.clear();
+                // mp[s[i]]++;
+                    break;
+            }
+            ans = max(ans ,cnt);
+            }
+        }
+        return ans;
+    }
+};
---
+++ Downloads/LC_Submissions/longest-substring-without-repeating-characters/Wrong Answer/2-22-2023, 1:58:01 PM/Solution.cpp
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/longest-substring-without-repeating-characters
+
+class Solution {
+public:
+    
+    
+//     a b c a d e f g h 
+    int lengthOfLongestSubstring(string s) {
+        map<char,int> mp;
+        int cnt =0;
+        int ans =0;
+        for(int i=0;i<s.length();i++){
+            for(int j=i;j<s.length();j++){
+                if(mp.find(s[i])==mp.end()){
+                mp[s[i]]++;
+                cnt++;
+            }else {
+                // cnt =0;
+                // cnt++;
+                // mp.clear();
+                // mp[s[i]]++;
+                    break;
+            }
+            ans = max(ans ,cnt);
+            }
+        }
+        return ans;
+    }
+};
