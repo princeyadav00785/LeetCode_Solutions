76ead85191195e34abe24d1f3b4c084ab92e8611
PRINCE YADAV <100501934+princeyadav00785@users.noreply.github.com>
2024-03-17 17:09:00 +0530

    Time: 108 ms (19.39%) | Memory: 86.9 MB (90.50%) - LeetSync

--- 525-contiguous-array/contiguous-array.cpp
+++ 525-contiguous-array/contiguous-array.cpp
@@ -1,33 +1,20 @@
 class Solution {
 public:
     int findMaxLength(vector<int>& nums) {
-        // int z=0,o=0, ans=0;
-        // map<int,int>mp;
-        // mp[0]=-1;
-        // for(int i=0;i<nums.size();i++){
-        //  nums[i]==0 ?++z:++o;
-        //     int df=z-o;
-        //     if(mp.find(df)!=mp.end()){
-        //         ans=max(ans,i-mp[df]);
-        //     }
-        // }
-        // return ans;
-         unordered_map<int, int> hashmap;
-        int zeros = 0, ones = 0, max_len = 0;
-        
-        hashmap[0] = -1;
-        
-        for (int i = 0; i < nums.size(); ++i) {
-            nums[i] == 0 ? ++zeros : ++ones;
-            int diff = zeros - ones;
-            
-            if (hashmap.count(diff)) {
-                max_len = max(max_len, i - hashmap[diff]);
+        int z=0,o=0, ans=0;
+        map<int,int>mp;
+        mp[0]=-1;
+        for(int i=0;i<nums.size();i++){
+         nums[i]==0 ?++z:++o;
+            int df=z-o;
+            if(mp.find(df)!=mp.end()){
+                ans=max(ans,i-mp[df]);
             } else {
-                hashmap[diff] = i;
+                mp[df] = i;
             }
-        }
         
-        return max_len;
+        }
+        return ans;
+
     }
 };
