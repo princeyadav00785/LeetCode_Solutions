ac4ae1329c00e639bf8b0fac4c1b002a07255fa5
PRINCE YADAV <100501934+princeyadav00785@users.noreply.github.com>
2024-03-22 22:47:48 +0530

    Time: 378 ms (5.05%) | Memory: 130.8 MB (18.21%) - LeetSync

---
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+    public:
+    vector<int> traversal(ListNode* head){
+        vector<int> v;
+        ListNode* temp=head;
+        if(head == NULL){
+            return v;
+        }
+        while(temp!=NULL){
+            v.push_back(temp->val);
+            temp=temp->next;
+        }
+        return v;
+    }
+
+    bool isPalindrome(ListNode* head) {
+
+        vector<int> v1= traversal(head);
+        for(int i=0;i<v1.size();i++){
+            cout<<v1[i]<<" ";       }
+        for(int i=0;i<v1.size()/2;i++){
+           if(v1[i]!=v1[v1.size()-1-i]){
+               return false;
+           }    
+          
+        }
+        return true;
+       
+    }
+};
